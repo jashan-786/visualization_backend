5783188b1de849f15de3c3a5509cb9c46bec4508
jashanpreet <jashanpreet28122001@gmail.com>
2025-04-29 13:20:23 -0500

    secrets updated

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 import { AdminModel, adminSchema, connectionSchema } from "./models";
-
+import dotenv from "dotenv";
+dotenv.config();
 const connectdb = async () => {
   try {
     const con = await mongoose.connect(
-      `mongodb+srv://hsiskesks:AT2HQJgdC0NksiB3@cluster0.eggov.mongodb.net/visualization` as string
+      `mongodb+srv://hsiskesks:${process.env.MONGO_PASS}@cluster0.eggov.mongodb.net/visualization` as string
     );
 
     console.log(`MongoDB connected: ${con.connection.host}`);
@@ -16,7 +17,7 @@ const connectdb = async () => {
 export const connectbackupdb = async () => {
   try {
     const con = await mongoose.createConnection(
-      `mongodb+srv://hsiskesks:AT2HQJgdC0NksiB3@cluster0.eggov.mongodb.net/visualizationbackup` as string
+     `mongodb+srv://hsiskesks:${process.env.MONGO_PASS}@cluster0.eggov.mongodb.net/visualizationbackup` as string
     );
 
     console.log(`MongoDB connected backup:`);
